652823a4ab1228f599856bef691a06b9cc5dd0dc
lmaosoggypancakes <lmaosoggywaffles@gmail.com>
2020-12-15 10:58:03 -0800

    b

--- classroom/static/classroom/class.js
+++ classroom/static/classroom/class.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket(
         'wss://'
         + window.location.host
-        + '/ws/chat/'
+        + '/wss/chat/'
         + window.location.pathname[7]
         + '/'
     );
