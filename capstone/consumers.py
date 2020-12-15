c635ed51b6553d677a6f7c17b9822061b98cdbf4
lmaosoggypancakes <lmaosoggywaffles@gmail.com>
2020-12-15 11:22:06 -0800

    bruh

--- capstone/consumers.py
+++ capstone/consumers.py
@@ -1,27 +1,28 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
-        )
+        ))
 
-        await self.accept()
+        return self.accept()
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
-        )
+        ))
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         try:
             message = text_data_json['message']
@@ -30,27 +31,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
             message = text_data_json["annoucement"]
             ok = "annoucement"
         # Send message to room group
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'announcement',
                 ok: message
             }
-        )
+        ))
 
     # Receive message from room group
-    async def chat_message(self, event):
+    def chat_message(self, event):
         try:
             message = event['message']
         except KeyError:
             message = event['annoucement']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        async_to_sync(self.send(text_data=json.dumps({
             'message': message
-        }))
+        })))
 
     # receive announcement from class   
-    async def announcement(self, event):
+    def announcement(self, event):
         try:
             message = event["annoucement"]
             ok = "annoucement"
@@ -58,6 +59,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             message = event["message"]
             ok = "message"
 
-        await self.send(text_data=json.dumps({
+        async_to_sync(self.send(text_data=json.dumps({
             ok: message
-        }))
+        })))
